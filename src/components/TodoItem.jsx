ffcf54a2a0d9c350866dd6cec4732a08bfe27342
= <100366113+AnaMontanoM@users.noreply.github.com>
2023-04-22 18:46:39 -0500

    Challenge09 (el primerito del segundo corte)

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const TodoItem = ({ todo }) => {
+	return (
+		<li className="list-group-item d-flex justify-content-between">
+			<span> {todo.description} </span>
+		</li>
+	);
+};
