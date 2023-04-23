20e0fe0f5e9ddce0680e504f244029ef079963db
= <100366113+AnaMontanoM@users.noreply.github.com>
2023-04-22 21:07:53 -0500

    Challenge16

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
-import { Provider } from "./components/Context";
+import { Provider } from "./components/context";
 import { PrivateRoutes } from "./components/PrivateRoutes";
 import { LoginPage } from "./components/LoginPage";
 import { Homepage } from "./components/Homepage";
